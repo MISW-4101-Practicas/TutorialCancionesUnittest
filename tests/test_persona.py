6387976c3c6502e502fef7a72ecede90805fc580
lrozoq <l.rozoq@uniandes.edu.co>
2023-03-11 19:08:24 -0500

    actions con pull request

--- tests/test_persona.py
+++ tests/test_persona.py
@@ -6,7 +6,7 @@ from src.modelo.persona import Persona
 class PersonaTestCase(unittest.TestCase):
 
    def setUp(self):
-      self.persona1 = Persona(nombre='Alejandra', edad=25)
+      self.persona1 = Persona(nombre='Alejandra', edad=26)
       self.persona2 = Persona(nombre='Diego', edad=22)
       self.persona3 = Persona(nombre='Alejandra', edad=25)
       self.persona4 = Persona(nombre='Diana', edad=25)
